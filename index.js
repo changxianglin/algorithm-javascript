d016240d25e0369ae3dee37138dec8cf9e37d903
changxianglin <281532612@qq.com>
2021-04-21 17:02:00 +0800

    queue

--- index.js
+++ index.js
@@ -242,3 +242,130 @@ const log = console.log.bind(console)
 //   }
 // }
 
+// create Queue
+// function Queue() {
+//   let items = []
+
+//   this.enqueue = function(element) {
+//     items.push(element)
+//   }
+//   this.dequeue = function () {
+//     return items.shift()
+//   }
+//   this.front = function () {
+//     return items[0]
+//   }
+//   this.isEmpty = function () {
+//     return items.length == 0
+//   }
+//   this.size = function () {
+//     return items.length
+//   }
+//   this.print = function () {
+//     log(items.toString())
+//   }
+// }
+
+// let queue = new Queue()
+// log(queue.isEmpty()) // true
+
+// queue.enqueue('John')
+// queue.enqueue('Jack')
+
+// queue.enqueue('Camila')
+// queue.print() // John Jack Camaila
+// log(queue.size()) // 3
+// log(queue.isEmpty()) // false
+// queue.dequeue()
+// queue.dequeue()
+// queue.print() // Camila
+
+
+// class Queue
+// let Queue2 =  (function() {
+//   let items = new WeakMap()
+
+//   class Queue2{
+//     constructor() {
+//       items.set(this, [])
+//     }
+//     enqueue(element) {
+//       let q = items.get(this)
+//       q.push(element)
+//     }
+//     dequeue() {
+//       let q = items.get(this)
+//       return q.shift()
+//     }
+//     front() {
+//       let q = items.get(this)
+//       return q[0]
+//     }
+//     isEmpty() {
+//       let q = items.get(this)
+//       return q.length == 0
+//     }
+//     size() {
+//       let q = items.get(this)
+//       return q.length
+//     }
+//     print() {
+//       let q = items.get(this)
+//       log(q.toString())
+//     }
+//   }
+
+//   return Queue2
+// })()
+
+// let queue = new Queue2()
+// log(queue.isEmpty()) // true
+
+// queue.enqueue('John')
+// queue.enqueue('Jack')
+
+// queue.enqueue('Camila')
+// queue.print() // John Jack Camaila
+// log(queue.size()) // 3
+// log(queue.isEmpty()) // false
+// queue.dequeue()
+// queue.dequeue()
+// queue.print() // Camila
+
+
+// function PriorityQueue() {
+//   let items = []
+//   function QueueElement(element, priority) {
+//     this.element = element
+//     this.priority = priority
+//   }
+
+//   this.enqueue = function(element, priority) {
+//     let queueElement = new QueueElement(element, priority)
+
+//     let added = false
+//     for (let i = 0; i < items.length; i++) {
+//       if (queueElement.priority < items[i].priority) {
+//         items.splice(i, 0, queueElement)
+//         added = true
+//         break
+//       }
+//     }
+//     if (!added) {
+//       items.push(queueElement)
+//     }
+//   }
+//   this.print = function () {
+//     for (let i = 0; i < items.length; i++) {
+//       log(`${items[i].element} - ${items[i].priority}`)
+//     }
+//   }
+// }
+
+// let priorityQueue = new PriorityQueue()
+// priorityQueue.enqueue('John', 2)
+// priorityQueue.enqueue('Jack', 1)
+// priorityQueue.enqueue('Camila', 1)
+// priorityQueue.print()
+
+
