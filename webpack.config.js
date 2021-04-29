6fb1746c434b14bb2390fc9fda0dec037e998702
changxianglin <281532612@qq.com>
2021-04-29 15:03:50 +0800

    module

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './demo1.js',
+  entry: './demo2.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
